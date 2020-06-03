d2139f672ea27a57bad94f87f1c2215bbab856c2
Brian Monaccio <brianmonaccio@gmail.com>
2020-06-03 19:11:23 -0400

    Completes brute force approach

--- sumEvenFibonacciNumbers.js
+++ sumEvenFibonacciNumbers.js
@@ -1,5 +1,17 @@
 function sumEvenFibonacciTerms(limit){
+  var sum = 0;
+  var fib1 = 2;
+  var fib2 = 3;
+  var temp;
   
+  while (fib1 <= limit){
+    if (fib1 % 2 === 0) sum += fib1;
+    temp = fib1;
+    fib1 = fib2;
+    fib2 = temp + fib2
+  }
+
+  return sum;
 }
 
 
