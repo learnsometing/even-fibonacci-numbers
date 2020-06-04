563935a7a7d9ce1cf1d9b37c3ce18558e4f00042
Brian Monaccio <brianmonaccio@gmail.com>
2020-06-03 20:39:52 -0400

    Implements an optimized solution to achieve constant time complexity.

--- sumEvenFibonacciNumbers.js
+++ sumEvenFibonacciNumbers.js
@@ -1,18 +1,33 @@
-function sumEvenFibonacciTerms(limit){
-  var sum = 0;
-  var fib1 = 2;
-  var fib2 = 3;
-  var temp;
-  
-  while (fib1 <= limit){
-    if (fib1 % 2 === 0) sum += fib1;
-    temp = fib1;
-    fib1 = fib2;
-    fib2 = temp + fib2
-  }
+const SQ5 = Math.sqrt(5);
+const PHI = (SQ5 + 1) / 2;
+const PSI = (SQ5 - 1) / 2;
 
-  return sum;
+function calcNthTerm(n){
+  // returns the nth fibonacci term calculated using Binet's formula
+
+  return Math.round((Math.pow(PHI, n) - Math.pow(PSI, n)) / SQ5);
+}
+
+function getIndexClosestToLimit(limit){
+  // returns the index of the fibonacci term closest to the limit
+  // i = (log(limit) + log(sqrt(5)))/ log(phi) where phi = log(sqrt(5 + 1) / 2)
+  
+  return Math.floor((Math.log(limit) + Math.log(SQ5)) / Math.log(PHI));
+}
+
+function sum(n){
+  return calcNthTerm(n + 2) - 1;
+}
+
+function sumEvenFibonacciTerms(){
+  //steps
+    // 1. calculate the index of the fib term closest to the limit
+    // 2. calculate the sum of fibonacci terms using closed form summation expression
+    // 3. divide result by 2.
+
+  var idx = getIndexClosestToLimit(4e6);
+  return sum(idx) / 2;
 }
 
 
-module.exports = sumEvenFibonacciTerms;
+module.exports = {calcNthTerm, getIndexClosestToLimit, sum, sumEvenFibonacciTerms};
--- sumEvenFibonacciNumbers.spec.js
+++ sumEvenFibonacciNumbers.spec.js
@@ -1,8 +1,25 @@
-const sumEvenFibonacciTerms = require('./sumEvenFibonacciTerms');
+const {calcNthTerm, sum, getIndexClosestToLimit, sumEvenFibonacciTerms} = require('./sumEvenFibonacciNumbers');
+
+describe('getIndexClosestToLimit', () => {
+  it('should return the correct index in the fibonacci sequence of the limit', () => {
+    expect(getIndexClosestToLimit(4000000)).toEqual(33);
+  });
+});
+
+describe('calcNthTerm', () => {
+  it('should return the correct fibonacci number', () => {
+    expect(calcNthTerm(33)).toEqual(3524578);
+  });
+})
+
+describe('sum', () => {
+  it('should return the sum of all fibonacci numbers from 0 to n', () => {
+    expect(sum(9)).toEqual(88);
+  });
+});
 
 describe('sumEvenFibonacciTerms', () => {
   it("should sum even fibonacci numbers whose values are below four million", () => {
-    // I googled what the result should be to write the test
-    expect(sumEvenFibonacciTerms(4000000)).toEqual(4613732);
+    expect(sumEvenFibonacciTerms()).toEqual(4613732);
   });
 });
